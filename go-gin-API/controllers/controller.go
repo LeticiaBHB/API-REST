cd328e0311ddd491acdfa291f4182a865d5722a2
bowie <leticia.bini@gmail.com>
2023-12-09 08:43:22 -0300

    commit - db and postman

--- go-gin-API/controllers/controller.go
+++ go-gin-API/controllers/controller.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/LeticiaBHB/Go-gin-API/database"
 	"github.com/LeticiaBHB/Go-gin-API/models"
 	"github.com/gin-gonic/gin"
 )
 
 func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, models.Alunos)
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.JSON(200, alunos)
 }
 
 func Saudacao(c *gin.Context) {
@@ -14,4 +19,15 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", beleza ?",
 	})
+}
+
+func CriaNovoAluno(c*gin.Context){
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+			return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- go-gin-API/main.go
+++ go-gin-API/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"github.com/LeticiaBHB/Go-gin-API/models"
+	"github.com/LeticiaBHB/Go-gin-API/database"
 	"github.com/LeticiaBHB/Go-gin-API/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno{
-		{Nome: "Joana", CPF:"0000000000", RG: "1234567890"},
-		{Nome: "Ana", CPF:"11111111111", RG: "0987654321"},
-	}
+	database.ConectaComBancoDeDados()
 	routes.HandleRequests()
 }
 
